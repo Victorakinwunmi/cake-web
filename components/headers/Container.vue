62464fed63a8e740976bdc172105f952c6b4f6db
Victorakinwunmi <adexvictor94@gmail.com>
2022-08-31 18:23:15 -0700

    i added a header section

---
+++ components/headers/Container.vue
@@ -0,0 +1,83 @@
+<template>
+  <div
+    class="
+      flex
+      justify-between
+      bg-pink-300
+      w-full
+      p-4
+      shadow-xl
+      fixed
+      items-center
+    "
+  >
+    <div>
+      <slot name="logo" />
+    </div>
+
+    <div class="">
+      <div
+        :class="is_revealed ? 'hidden' : 'block lg:hidden'"
+        @click="showMenu"
+        class="p-4"
+      >
+        <div class="">
+          <slot name="hamburgermenuopen"></slot>
+        </div>
+      </div>
+
+      <div
+        :class="is_revealed ? 'block' : 'hidden lg:hidden'"
+        @click="hideMenu"
+        class="p-4"
+      >
+        <div>
+          <slot name="hamburgermenuclose"></slot>
+        </div>
+      </div>
+    </div>
+    <div
+      :class="is_revealed ? 'block top-14 right-0 w-full h-screen mt-11 absolute bg-pink-300 ' : 'hidden lg:flex'"
+      class="flex"
+    >
+      <div>
+        <slot name="menu" />
+      </div>
+
+    </div>
+    <div class="hidden lg:flex">
+        <div>
+          <slot name="search" />
+        </div>
+
+        <div>
+          <slot name="shop" />
+        </div>
+
+        <div>
+          <slot name="button" />
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      is_revealed: false,
+    };
+  },
+  methods: {
+    showMenu() {
+      this.is_revealed = true;
+    },
+    hideMenu() {
+      this.is_revealed = false;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ components/headers/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="lg:flex text-black font-semibold">
+      <nuxt-link to="www.pahh.com" class="block mt-10 mb-1  mx-8 lg:mx-4 lg:py-1 lg:mt-0 lg:mb-0 hover:text-pink-600 border-b-4 hover:border-pink-600 border-transparent duration-500 ease-in-out"><slot name="hometext" /></nuxt-link>
+      <nuxt-link to="www.paath" class="block mt-5 mb-1  mx-8 lg:mx-4 lg:py-1 lg:mt-0 lg:mb-0 hover:text-pink-600 hover:text-pink-600 border-b-4 hover:border-pink-600 border-transparent duration-500 ease-in-out" ><slot name="servicetext" /></nuxt-link>
+      <nuxt-link to="www.pptth.com" class="block mt-5 mb-1  mx-8 lg:mx-4 lg:py-1 lg:mt-0 lg:mb-0 hover:text-pink-600 hover:text-pink-600 border-b-4 hover:border-pink-600 border-transparent duration-500 ease-in-out"><slot name="menutext" /></nuxt-link>
+      <nuxt-link to="www.paath.com" class="block mt-5 mb-1  mx-8 lg:mx-4 lg:py-1 lg:mt-0 lg:mb-0 hover:text-pink-600 hover:text-pink-600 border-b-4 hover:border-pink-600 border-transparent duration-500 ease-in-out"><slot name="contacttext" /></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+};
+</script>
+
+<style>
+</style>
