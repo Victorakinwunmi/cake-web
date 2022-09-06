de2ab68fee506b4d0135e71e05fb06959ad71d95
Victorakinwunmi <Adexvictor94@gmail.com>
2022-09-06 15:18:03 -0500

    i added margin-top to the cta image

--- components/cta/Image.vue
+++ components/cta/Image.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="block w-full xl:w-10/12 bg-pink-100 rounded-full border-2 mx-0 lg:mx-auto mt-28 lg:mt-10 ">
+    <div class="block w-full xl:w-10/12 bg-pink-100 rounded-full border-2 mx-0 lg:mx-auto mt-40 lg:mt-10 ">
          <img :src="require('@/assets/images/cta_2.png')" class="-mt-28 w-full">
     </div>
  
