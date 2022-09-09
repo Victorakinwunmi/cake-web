2bba0cb978dda07f3ea1591a632b6a89f3f7ba99
Victorakinwunmi <Adexvictor94@gmail.com>
2022-09-09 16:32:14 -0500

    i increase the text size in menupage cta

--- pages/menupage.vue
+++ pages/menupage.vue
@@ -54,8 +54,8 @@
         <div class="bg-no-repeat bg-cover bg-center" :style="{backgroundRepeat: 'no-repeat' + ';' , backgroundImage: 'url(' + require('@/assets/images/Cake04.jpg') +')'}">
           <div class="bg-gray-300 px-10 py-44 lg:py-60 bg-opacity-80 w-full">
              <div class="lg:px-16 max-w-3xl">
-               <h1 class="font-bold text-3xl md:text-4xl lg:text-7xl">Find perfect <span class="text-pink-500">cakes</span> and <span class="text-pink-500">bakes</span> company near you</h1>
-               <p class="text-base lg:text-xl mt-10">Take a look at our menus below, choose your favorite porducts and place an order</p>
+               <h1 class="font-bold text-5xl md:text-6xl lg:text-7xl">Find perfect <span class="text-pink-500">cakes</span> and <span class="text-pink-500">bakes</span> company near you</h1>
+               <p class="text-xl lg:text-xl mt-10">Take a look at our menus below, choose your favorite porducts and place an order</p>
                <div class="border border-black rounded-full w-44 mt-8 justify-center p-2 flex">
                   <a href="https://wa.me/+2348107546757"><img :src="require('@/assets/icons/whatsapp.png')" class="lg:w-10 w-8"></a>
                   <a href=""> <img :src="require('@/assets/icons/facebook.png')" class="lg:w-10 w-8"></a>
