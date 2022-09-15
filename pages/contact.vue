bb3675694e1f313bfea0cb0bdded99eee7905e5e
Victorakinwunmi <Adexvictor94@gmail.com>
2022-09-14 20:21:39 -0500

    i changed the lhs section text color in contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -61,7 +61,7 @@
             >
               <div class="bg-gray-300 px-10 h-screen py-44 lg:py-60 bg-opacity-50 w-full">
                 <div>
-                  <h1 class="font-bold text-6xl max-w-lg mx-auto leading-snug">We would love to hear from you</h1>
+                  <h1 class="font-bold text-6xl text-gray-800 max-w-lg mx-auto leading-snug">We would love to hear from you</h1>
                 </div>
               </div>
             </div>
